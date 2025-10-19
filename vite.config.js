a78a6ef47234f5fb4b180c6185e56c2da2b1ba2f
QeseQ123 <qeseq123@gmail.com>
2025-10-20 01:39:06 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '',        // katalog z index.html
   plugins: [react()],
-  build: {
-    outDir: '../dist' // folder wyjściowy na poziomie głównym
-  },
 });
