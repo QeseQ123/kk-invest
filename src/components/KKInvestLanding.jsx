48eed6882d014da53d2508d4c417a57aaedeebf0
QeseQ123 <qeseq123@gmail.com>
2025-10-20 22:43:22 +0200

    Added mobile version (buttons, etc.)

--- src/components/KKInvestLanding.jsx
+++ src/components/KKInvestLanding.jsx
@@ -29,9 +29,7 @@ const Nav = () => (
     <Link to="/o-nas" className="nav-link">O nas</Link>
     <Link to="/realizacje" className="nav-link">Realizacje</Link>
     <Link to="/kontakt" className="nav-link">Kontakt</Link>
-    <a className="ml-4 px-4 py-2 rounded-lg bg-gradient-to-r from-[#ffd166] to-[#ff6b6b] text-black font-semibold shadow-lg">
-      Zamów wycenę
-    </a>
+    <a className="ml-4 px-4 py-2 rounded-lg bg-gradient-to-r from-[#ffd166] to-[#ff6b6b] text-black font-semibold shadow-lg">Zamów wycenę</a>
   </nav>
 );
 
