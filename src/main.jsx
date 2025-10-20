c4bfc5228b9a9a427f8e0791580fb69564757720
QeseQ123 <qeseq123@gmail.com>
2025-10-20 18:59:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
+// src/main.jsx
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+import KKInvestLanding from './KKInvestLanding';
+import './index.css'; // Tailwind itp.
 
-
-createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<BrowserRouter>
-<App />
-</BrowserRouter>
-</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KKInvestLanding />
+  </React.StrictMode>
 );
